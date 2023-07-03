10483aa7d179d668f4fb8625a57f409dfe9feb04
AdityaGavali <adityagavali18@gmail.com>
2023-07-03 20:35:59 +0530

    ok

--- Binary_Search/Questions/first_last_element_in_a_sorted_array.cpp
+++ Binary_Search/Questions/first_last_element_in_a_sorted_array.cpp
@@ -2,7 +2,7 @@
 
 // If target is not found in the array, return [-1, -1].
 
-// You must write an algorithm with O(log n) runtime complexity.
+// You must write an algorithm with O(log n) runtime complexity..
 
 #include<bits/stdc++.h>
 using namespace std;
